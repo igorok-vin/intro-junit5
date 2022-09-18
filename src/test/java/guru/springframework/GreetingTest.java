f1ef0c2105013ab9b575ad7cfecb4152c84c27b7
Ihor <igorok.vin@gmail.com>
2022-09-17 20:41:58 -0600

    Lifecycle Demo

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,13 +1,17 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before - I'm only called Once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -23,5 +27,15 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Igor"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each Method...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After Everything - I'm only called Once!!!");
     }
 }
